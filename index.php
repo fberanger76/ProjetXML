b5227949e57917a3d6fef388e22b72cf39af35c3
Franck <franckberanger@iMac-de-Franck.local>
2021-06-28 15:22:05 +0200

    changement de titres ok

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 $xml = simplexml_load_file("assets/xml/source.xml");
 $list = $xml->page;
 $page = $_GET["page"];
+$title = $list[0]->title;
+if (isset($page) && $page == 1){
+    $title = $list[0]->title;
+    } else if (isset($page) && $page == 2) {
+        $title = $list[1]->title;
+    } else if (isset($page) && $page == 3) {
+        $title = $list[2]->title;
+    } else {
+        $title = $list[3]->title;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +21,13 @@ $page = $_GET["page"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Maçonnerie</title>
+    <title><?php echo $title ?></title>
 </head>
 <body>
 <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <div class="fw-bold">Maçonnerie Ocordo</div>
+    <div class="fw-bold fs-2">Maçonnerie Ocordo</div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -41,15 +51,15 @@ $page = $_GET["page"];
 </nav>
 
 <?php 
-if (isset($page) && $page == 1){
-echo $list[0]->content . '<br><br>';
-} else if (isset($page) && $page == 2) {
-    echo $list[1]->content . '<br><br>';
-} else if (isset($page) && $page == 3) {
-    echo $list[2]->content . '<br><br>';
-} else {
-    echo $list[3]->content . '<br><br>';
-}
+ if (isset($page) && $page == 1){
+ echo $list[0]->content . '<br><br>';
+ } else if (isset($page) && $page == 2) {
+     echo $list[1]->content . '<br><br>';
+ } else if (isset($page) && $page == 3) {
+     echo $list[2]->content . '<br><br>';
+ } else {
+     echo $list[3]->content . '<br><br>';
+ }
 // for ($i = 0; $i < count($list); $i++) {
 
 //     echo '<b>Numéro de page:</b> ' . $list[$i]->attributes()->id . '<br>';
